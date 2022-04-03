cc62dca7c3c6c8361356ea80af5761cb45ec2b00
Luke Spicknall <lukejspicknall@gmail.com>
2022-04-03 18:14:15 -0400

    intial grid js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const container = document.getElementById("container");
-let gridSize = 16
+let gridSize = 16;
 
-function createGrid () {
-    for(i = 0; i < (gridSize * gridsize); i++ ){
-        let cell = document.createElement('gridCell');
-        container.appendChild(cell);
+
+
+function createGrid() {
+    for(i = 0; i < (gridSize * gridSize); i++){
+        let cell = document.createElement('div');
+        container.appendChild(cell).className = 'grid-item';
     }
 }
+createGrid();
+//function makeRows(rowNum) {
+//    for (r = 0; r < rowNum; r++) {
+//        let row = document.createElement('div');
+//        container.appendChild(row).className = 'rblock';
+//    }
+//}
